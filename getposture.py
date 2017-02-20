5a5c4c824283deee19bca20d8d685aa5694ee8ae
jiexunsee <jxsee94@yahoo.com.sg>
2017-02-20 22:17:29 +0800

    master

--- getposture.py
+++ getposture.py
@@ -1,10 +1,9 @@
 import sys
 from RMPE.testing.python.config_reader import config_reader
-
 from RMPE.testing.python.demo import getposture
 
 param, model = config_reader()
 
 
-#modify this to loop through pictures, maybe
-getposture('pics/jeans.jpg', param, model)
+# Call this function on all photos with user's face. It will save the cropped clothes into somewhere.
+getposture('pics/group-photo-04.jpg', param, model)
